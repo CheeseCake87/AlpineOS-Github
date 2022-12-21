56b1fe0659447e2474b9e19de30a9048dc61f65f
David Carmichael <carmichaelits@gmail.com>
2022-12-21 15:38:23 +0000

    adjusted manual pull

--- pull.py
+++ pull.py
@@ -2,13 +2,10 @@ import os
 
 from dotenv import load_dotenv
 
-from setup import stop_supervisor, start_supervisor
 from utils.github import Github
 
 load_dotenv()
 
 github = Github(os.getenv("GIT"))
 
-stop_supervisor("repo")
 github.pull()
-start_supervisor("repo")
