3da08b7273a32d46bf95555489367f5a51b365db
david <carmichaelits@gmail.com>
2022-12-28 15:58:49 +0000

    Tweaked

--- github.py
+++ github.py
@@ -1,33 +1,12 @@
 import os
 import pathlib
 import subprocess as sp
+from typing import Optional, Union
 
 from git import Repo
 
 from dotenv import load_dotenv
 from flask import Flask
-
-
-class Github:
-    github_repo_url: str
-    repo_folder: pathlib.Path
-    repo: Repo
-
-    def __init__(self, github_repo: str):
-        self.github_repo_url = github_repo
-        self.repo_folder = pathlib.Path(pathlib.Path.cwd() / "repo")
-        self.repo_folder.mkdir(exist_ok=True)
-        self.setup()
-
-    def setup(self):
-        if len(os.listdir(self.repo_folder)) == 0:
-            self.repo = Repo.clone_from(self.github_repo_url, self.repo_folder)
-            sp.call([f'python3', '-m', 'venv', f'{self.repo_folder}/venv'])
-            sp.call([f'{self.repo_folder}/venv/bin/python3', '-m', 'pip', 'install', '-r',
-                     f'{self.repo_folder}/requirements.txt'])
-
-    def pull(self):
-        Repo(self.repo_folder).remotes.origin.pull()
 
 
 class AppController:
@@ -46,26 +25,44 @@ class AppController:
             sp.Popen.terminate(self.process)
 
 
+class Github:
+    github_repo_url: str
+    repo_folder: pathlib.Path
+    repo: Repo
+
+    def __init__(self, github_repo: str):
+        self.github_repo_url = github_repo
+        self.repo_folder = pathlib.Path(pathlib.Path.cwd() / "repo")
+        self.repo_folder.mkdir(exist_ok=True)
+
+    def setup(self, app_controller: AppController):
+        if len(os.listdir(self.repo_folder)) == 0:
+            self.repo = Repo.clone_from(self.github_repo_url, self.repo_folder)
+            sp.call([f'python3', '-m', 'venv', f'{self.repo_folder}/venv'])
+            sp.call([f'{self.repo_folder}/venv/bin/python3', '-m', 'pip', 'install', '-r',
+                     f'{self.repo_folder}/requirements.txt'])
+            app_controller.start()
+
+    def pull(self):
+        Repo(self.repo_folder).remotes.origin.pull()
+
+
 load_dotenv()
 
 git = os.getenv("GIT")
 
-github = Github(git)
-
 
 def create_app():
     app = Flask(__name__)
-
-    github.setup()
-
     app_controller = AppController()
+
+    github = Github(git)
+    github.setup(app_controller)
 
     # Index
     @app.get('/pull')
     def webhook():
-        app_controller.stop()
         github.pull()
-        app_controller.start()
         return 'pulled', 200
 
     @app.get('/start')
