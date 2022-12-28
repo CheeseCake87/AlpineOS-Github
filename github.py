ae333212987d176f667433fb0ed98fc075674650
david <carmichaelits@gmail.com>
2022-12-28 15:51:46 +0000

    Simplified the process

--- github.py
+++ github.py
@@ -1,10 +1,50 @@
 import os
+import pathlib
+import subprocess as sp
+
+from git import Repo
 
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask
 
-from setup import stop_supervisor, start_supervisor
-from utils.github import Github
+
+class Github:
+    github_repo_url: str
+    repo_folder: pathlib.Path
+    repo: Repo
+
+    def __init__(self, github_repo: str):
+        self.github_repo_url = github_repo
+        self.repo_folder = pathlib.Path(pathlib.Path.cwd() / "repo")
+        self.repo_folder.mkdir(exist_ok=True)
+        self.setup()
+
+    def setup(self):
+        if len(os.listdir(self.repo_folder)) == 0:
+            self.repo = Repo.clone_from(self.github_repo_url, self.repo_folder)
+            sp.call([f'python3', '-m', 'venv', f'{self.repo_folder}/venv'])
+            sp.call([f'{self.repo_folder}/venv/bin/python3', '-m', 'pip', 'install', '-r',
+                     f'{self.repo_folder}/requirements.txt'])
+
+    def pull(self):
+        Repo(self.repo_folder).remotes.origin.pull()
+
+
+class AppController:
+    process: sp.Popen = None
+
+    def start(self):
+        self.process = sp.Popen(
+            ['venv/bin/gunicorn', '-b', '0.0.0.0:5000', 'run:sgi'],
+            cwd=pathlib.Path(pathlib.Path.cwd() / "repo"),
+            stdout=sp.PIPE,
+            stderr=sp.PIPE,
+        )
+
+    def stop(self):
+        if hasattr(self.process, 'send_signal'):
+            sp.Popen.terminate(self.process)
+
 
 load_dotenv()
 
@@ -16,22 +56,32 @@ github = Github(git)
 def create_app():
     app = Flask(__name__)
 
+    github.setup()
+
+    app_controller = AppController()
+
     # Index
-    @app.post('/')
+    @app.get('/pull')
     def webhook():
-        if request.method == 'POST':
-            json = request.json
-            clone_url = json['repository']['clone_url']
-            if clone_url == git:
-                stop_supervisor("repo")
-                github.pull()
-                start_supervisor("repo")
-            return 'ok', 200
+        app_controller.stop()
+        github.pull()
+        app_controller.start()
+        return 'pulled', 200
+
+    @app.get('/start')
+    def start_app():
+        app_controller.start()
+        return 'started', 200
+
+    @app.get('/stop')
+    def stop_app():
+        app_controller.stop()
+        return 'stopped', 200
 
     return app
 
 
 if __name__ == "__main__":
-    create_app().run(port=5500, host="0.0.0.0")
+    create_app().run(port=5500, debug=True)
 else:
     sgi = create_app()
